c1eb4ea035ea635f4a1ea623c25f558feca99552
Ermogenes Palacio <ermogenes@gmail.com>
2020-03-25 13:04:36 -0300

    Implementa frontend (sem backend)

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import Register from '../Register';
 
 import './styles.css';
 
@@ -15,10 +18,10 @@ export default function Logon() {
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua Id" />
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link to="/register" className="back-link">
                         <FiLogIn size={16} color="#e02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes"/>
